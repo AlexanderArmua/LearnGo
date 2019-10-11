e3b95703e014b2a43218578851c4dfc4fea9118d
Alexander <alexanderarmua1@gmail.com>
2019-10-11 00:59:56 -0300

    Agregado calculo de ganador, cada cierto tiempo falla

--- TenisChannel.go
+++ TenisChannel.go
@@ -12,14 +12,12 @@ type Tenista struct {
 	Nombre              string
 }
 
-func (self *Tenista) Raquetazo(tenistaChannel chan bool) {
+func (self *Tenista) Raquetazo() bool {
 	// TODO: Evitar creer seed cada vez que llaman a la función.
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 
-	acerto := self.ProbabilidadAcierto > r1.Float32()
-
-	tenistaChannel <- acerto
+	return self.ProbabilidadAcierto > r1.Float32()
 }
 
 func main() {
@@ -38,46 +36,54 @@ func main() {
 		ganadorTenisChannel <- nombreGanador
 	}()
 
-	fmt.Println("Ganador:", <-ganadorTenisChannel)
+	fmt.Print(<-ganadorTenisChannel)
 }
 
-func comenzarPartido(tenista1 *Tenista, tenista2 *Tenista) string {
-	var tenistaChannel chan bool = make(chan bool)
+func jugarLosTantos(t1 *Tenista, t2 *Tenista, c chan *Tenista) {
 	var puntosParaTerminarPartido int = 5
-	var nombreGanador string
 
-	for {
-		go func() {
-			tenista1.Raquetazo(tenistaChannel)
+	tenistas := [2]*Tenista{t1, t2}
 
-			if !<-tenistaChannel {
-				tenista2.Puntos++
-			}
-		}()
+	for i := 0; i < (2 * puntosParaTerminarPartido); i++ {
+		nroTenista := i % 2
 
-		go func() {
-			tenista2.Raquetazo(tenistaChannel)
-
-			if !<-tenistaChannel {
-				tenista1.Puntos++
-			}
-		}()
-
-		// Calcular si alguien gano despues de cada raquetazo
-		tenistaGanador, ganoAlguien := getTenistaGanador(tenista1, tenista2, puntosParaTerminarPartido)
-		if ganoAlguien {
-			nombreGanador = tenistaGanador.Nombre
-			break
+		if !tenistas[nroTenista].Raquetazo() {
+			c <- tenistas[(i+1)%2]
 		}
 	}
 
-	return nombreGanador
+	// Borrar
+	close(c)
 }
 
-func getTenistaGanador(tenista1 *Tenista, tenista2 *Tenista, puntosRequeridos int) (*Tenista, bool) {
-	if tenista1.Puntos == puntosRequeridos {
+func comenzarPartido(tenista1 *Tenista, tenista2 *Tenista) string {
+	var tenistaChanel = make(chan *Tenista)
+
+	retornar := ""
+	go jugarLosTantos(tenista1, tenista2, tenistaChanel)
+
+	for {
+		tenistaQueSumaPuntos, ok := <-tenistaChanel
+
+		if ok == false {
+			// Calcular quien se pego mas raquetazos
+			tenistaGanador, ganoAlguien := getTenistaGanador(tenista1, tenista2)
+			if ganoAlguien {
+				retornar = tenistaGanador.Nombre
+				break
+			}
+		} else {
+			tenistaQueSumaPuntos.Puntos++
+		}
+	}
+
+	return retornar
+}
+
+func getTenistaGanador(tenista1 *Tenista, tenista2 *Tenista) (*Tenista, bool) {
+	if tenista1.Puntos > tenista2.Puntos {
 		return tenista1, true
-	} else if tenista2.Puntos == puntosRequeridos {
+	} else if tenista2.Puntos > tenista1.Puntos {
 		return tenista2, true
 	}
 
